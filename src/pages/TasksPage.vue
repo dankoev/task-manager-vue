cc8ed172f5ff23877e4edc1801a1a20a30eaf7f7
Anigirka <daniil.00.kov@gmail.com>
2024-03-07 23:40:05 +0700

    added pages dummies

---
+++ src/pages/TasksPage.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <h1>Tasks</h1>
+  <form>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet totam nobis molestiae labore.
+    Delectus, inventore? Atque quos ipsa, sequi nisi, ad dolorem excepturi praesentium earum
+    repellat tempore sint facilis nulla!
+  </form>
+  <section>
+    <h3>Current tasks:</h3>
+  </section>
+</template>
+
+<style scoped></style>
