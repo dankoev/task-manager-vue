a8d5b4bbcde2c18eb7653483c83cbb6010744db7
Anigirka <daniil.00.kov@gmail.com>
2024-03-07 22:10:26 +0700

    connect to fonts and icons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
-<script setup></script>
+<script setup>
+import MainNav from './components/MainNav.vue'
+</script>
 
 <template>
-  <header></header>
-
+  <MainNav />
   <main></main>
 </template>
 
 <style scoped></style>
+<style>
+:root {
+  --text-color: rgba(54, 53, 53, 0.918);
+}
+
+* {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 300;
+  font-size: large;
+  color: var(--text-color);
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
